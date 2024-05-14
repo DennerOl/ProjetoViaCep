b35edfe2f19a6ed047d88dc5bff1bb3888f5d000
DennerOl <dener155@hotmail.com>
2024-05-13 21:09:45 -0300

    Seleção do form e func address p/ card

---
+++ projetoViaCep/js/controllers/form-controller.js
@@ -0,0 +1,35 @@
+import Address from "../models/address.js";
+
+// responsavel por tratar eventos do usuario controla o form.
+
+// função construtora para iniciar o estado dos atributos
+function State() {
+  this.address = new Address();
+
+  this.btnSave = null;
+  this.btnClear = null;
+
+  this.inputCep = null;
+  this.inputStreet = null;
+  this.inputNumber = null;
+  this.inputCity = null;
+
+  this.errorCep = null;
+  this.errorNumber = null;
+}
+
+const state = new State();
+
+export function init() {
+  // faço a seleção dos atributos do form
+  state.inputCep = document.forms.newAddress.cep;
+  state.inputStreet = document.forms.newAddress.street;
+  state.inputNumber = document.forms.newAddress.number;
+  state.inputCity = document.forms.newAddress.city;
+
+  state.btnSave = document.forms.newAddress.btnSave;
+  state.btnClear = document.forms.newAddress.btnClear;
+
+  state.errorCep = document.querySelector('[data-error="cep"]');
+  state.errorNumber = document.querySelector('[data-error="number"]');
+}
