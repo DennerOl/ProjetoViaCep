c52b1865daafbba3f6731781594784efde5f8428
DennerOl <dener155@hotmail.com>
2024-05-15 20:25:48 -0300

    Finalizando o modal

---
+++ projetoViaCep/js/controllers/modal-controller.js
@@ -0,0 +1,30 @@
+const state = new State();
+
+export function init() {
+  state.container = document.querySelector("#modal-contact");
+  state.btnClose = document.querySelector("#modal-contact-close");
+
+  state.btnClose.addEventListener("click", handleBtnCloseClick);
+  state.container.addEventListener("click", handleContainerClick);
+}
+
+export function showModal() {
+  state.container.classList.add("active");
+}
+
+export function closeModal() {
+  state.container.classList.remove("active");
+}
+
+function handleBtnCloseClick(event) {
+  event.preventDefault();
+  closeModal();
+}
+
+//função para fechar modal clicando fora
+function handleContainerClick(event) {
+  event.preventDefault();
+  if (event.target === this) {
+    closeModal();
+  }
+}
